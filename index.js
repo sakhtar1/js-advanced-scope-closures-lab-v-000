377684cb9843b71021c02aa99fa2841d0794ce35
Sharmina Akhtar <sakht318@gmail.com>
2018-08-08 17:26:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function produceDrivingRange(blockRange) {
+  return function(num1, num2){
+    let drivingDis = Math.abs(parseInt(num2) - parseInt(num1));
+    let range = Math.abs(drivingDis - blockRange);
+    if (drivingDis > blockRange) {
+      return `${range} blocks out of range`
+    } else {
+      return `within range by ${range}`
+    };
+  };
+};
+function produceTipCalculator(tip){
+  return function(bill){
+     return tip * bill;
+  }
+}
